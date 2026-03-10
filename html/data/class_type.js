1a3db8bf6fdb01f8409c9dd2ea05153c8e378c20
ytanneau <tannyann29@gmail.com>
2026-03-10 16:07:55 +0100

    update facture

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -1,18 +1,64 @@
 class Type {
-    
+
+    // inisalisalise la variable all_types
     static init_all_types() {
         let res = [];
-        
-        pokemon_types.forEach(element => {
-            res.push(new Type());
-        });
 
+        for (const [key, value] of Object.entries(type_effectiveness)) {
+            //console.log(`${key}: ${value}`);
+            res.push(new Type(key, value));
+        }
+        //console.table(res);
         return res;
     }
-    
+
     static all_types = Type.init_all_types();
 
-    constructor(objet){
-        this.nom = objet.
+    constructor(type, objet) {
+        this.nom = type;
+
+        let res = []
+
+        //recupère les différent coeff
+        let liste_coef = []
+        for (const [key, coef] of Object.entries(objet)) {
+            //console.log(`${key}: ${coef}`);
+            if (liste_coef.every(element => {
+                return coef != element;
+            })) {
+                liste_coef.push(coef);
+            }
+        }
+        //console.table(liste_coef);
+
+        // parcour les coeff
+        liste_coef.forEach(element => {
+            let ligne_coef = [element];
+
+            // parcour des type (avec leur coef)
+            let ligne_type = [];
+            for (const [key, coef] of Object.entries(objet)) {
+                //console.log(`${key}: ${coef}`);
+
+                // comparaison des coefs
+                if (element == coef) {
+                    ligne_type.push(key);
+                }
+            }
+            ligne_coef.push(ligne_type);
+            res.push(ligne_coef);
+        });
+
+        this.liste = res;
     }
+
+    toString() {
+        let message = `${this.nom} : `;
+
+        this.liste.forEach(element => {
+            message += element[0] + ' = [' + element[1].join(', ') + '], ';
+        } );
+        return message.slice(0, -2);
+    }
+
 }
