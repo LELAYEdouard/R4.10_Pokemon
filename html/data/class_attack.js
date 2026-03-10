3bc8c05c3378d482e107bedf38129235a42aa454
edlelay <edlelay@pce-test>
2026-03-10 15:31:04 +0100

    comit

--- html/data/class_attack.js
+++ html/data/class_attack.js
@@ -1,6 +1,35 @@
 class Attack {
+    id;
+    nom;
+    type;
+    puissance;
+    duree;
+    static all_attacks = {};
 
-    static toString() {
-        
+    constructor(i,n,t,p,d){
+        this.id = i;
+        this.nom=n;
+        this.type=t;
+        this.puissance=p;
+        this.duree=d;
+    }
+    toString(){
+        return `${this.nom} : #${this.id}, ${this.type}, ${this.puissance}, ${this.duree}ms`;
+    }
+
+    static fill_attacks(){
+
+        fast_moves.forEach(element => {
+            this.all_attacks[element.move_id] = new Attack(element.move_id,element.name,element.type,element.power,element.duration);
+        });
+
+        charged_moves.forEach(element => {
+            this.all_attacks[element.move_id] = new Attack(element.move_id,element.name,element.type,element.power,element.duration);
+        });
+
+       
     }
 }
+
+Attack.fill_attacks()
+console.log(Attack.all_attacks["13"].toString());
---
+++ html/data/class_pokemon.js
@@ -0,0 +1,31 @@
+class Pokemon{
+    id;
+    nom;
+    stamina;
+    base_att;
+    base_def;
+    types;
+    att_rap;
+    att_charg;
+    
+    static all_pokemons;
+
+    constructor(i,n,s,ba,bd,t,ar,ac){
+        this.id=i;
+        this.nom=n;
+        this.stamina=s;
+        this.base_att=ba;
+        this.base_def=bd;
+        this.types=t;
+        this.att_rap=ar;
+        this.att_charg=ac;
+    }
+
+    toString(){ 
+        return `${this.nom} : #${this.id}, [${this.types}], [STA: ${this.stamina},ATK: ${this.base_att},DEF:${this.base_def}], Rapides = [${this.att_rap}], Chargées = [${this.att_charg}]`;
+    }
+
+    static all_pokemons(){
+
+    }
+}
